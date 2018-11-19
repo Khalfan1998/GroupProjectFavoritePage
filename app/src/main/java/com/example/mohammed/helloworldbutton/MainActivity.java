83cb9d3cd6b913a1c8076e5cac68805683da90ba
Khalfan1998 <1607126@brunel.ac.uk>
2018-11-19 00:35:09 +0000

    My first Commit

---
+++ app/src/main/java/com/example/mohammed/helloworldbutton/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mohammed.helloworldbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt=(TextView) findViewById(R.id.mytext);
+        btn=(Button) findViewById(R.id.mybutton);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                txt.setText("Hello World");
+            }
+        });
+
+
+
+    }
+}
